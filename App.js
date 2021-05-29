f41dfbc7e1fb4d22d0b6cf8540f24ac656f1eaa3
PatrikBeichner <patrik.beichner@gmail.com>
2021-05-29 16:31:59 -0400

    cleanup + folders

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Entypo name="aircraft" size={20} color="green" />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

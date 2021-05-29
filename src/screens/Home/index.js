ddbc2dccc61c6d23f50710787d927f566de9acc4
PatrikBeichner <patrik.beichner@gmail.com>
2021-05-29 16:45:20 -0400

    images and homescreen init

---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = props => {
+    return (
+        <View>
+            <Text>not just a component</Text>
+            
+        </View>
+    )
+}
+
+
+export default index

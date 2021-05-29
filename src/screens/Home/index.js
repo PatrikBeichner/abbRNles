9933b6928829970ae74ecff5134b4fa6814c23e8
PatrikBeichner <patrik.beichner@gmail.com>
2021-05-29 17:20:14 -0400

    formatting changes

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import { 
-    View, 
-    ImageBackground, 
-    Text, 
-    Pressable } from 'react-native';
+import { View, ImageBackground, Text, Pressable } from 'react-native';
 import styles from './styles';
 
-const HomeScreen = props => {
-    return (
-        <View>
-            {/* Search bar */}
-            <ImageBackground 
-              source={require('../../../assets/images/wallpaper.jpg')} 
-              style={styles.image}
-            >
-                <Text style={styles.title}>Go Near</Text>
-                <Pressable 
-                  style={styles.button}
-                  onPress={()=>console.warn(data='Explore btn clicked')}
-                //   onPress={() => console.warn( data: 'Explore Btn clicked')}
-                >
-                    <Text style={styles.buttonText}>Explore nearby stays</Text>
-                </Pressable>
-            </ImageBackground>
-        </View>
-    )
-}
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Pressable
+        style={styles.searchButton}
+        onPress={() => console.warn((data = 'Search btn clicked'))}
+        //   onPress={() => console.warn( data: 'Explore Btn clicked')}
+      >
+        <Text style={styles.buttonText}>Explore nearby stays</Text>
+      </Pressable>
+      <ImageBackground source={require('../../../assets/images/wallpaper.jpg')} style={styles.image}>
+        <Text style={styles.title}>Go Near</Text>
+        <Pressable style={styles.button} onPress={() => console.warn('Explore Btn clicked')}>
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  );
+};
 
-
-export default HomeScreen
+export default HomeScreen;

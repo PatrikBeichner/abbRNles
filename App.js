a3fb5cfcdacabae68ac1e5812da4ac58616bc6da
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-02 17:50:22 -0400

    safeareaview fix

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import HomeScreen from './src/screens/Home';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
       <HomeScreen />
-    </View>
+    </SafeAreaView>
   );
 }

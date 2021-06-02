e0d4ec65211f27aa0acc2082f8746a4e0ec2b888
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-02 18:07:03 -0400

    post screen + styles init

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import HomeScreen from './src/screens/Home';
+import Post from './src/components/post';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <SafeAreaView>
       <HomeScreen />
+      <Post />
     </SafeAreaView>
   );
 }
---
+++ src/components/post/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles.js';
+
+const Post = (props) => {
+    return (
+        <View>
+            <Text>not just a component</Text>
+        </View>
+    );
+};
+
+export default Post;

220ceb363433412bb9dff4d9237194f95fb80f82
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-03 21:36:37 -0400

    post screen elements

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -4,13 +4,25 @@ import styles from './styles.js';
 
 const Post = (props) => {
     return (
-        <View>
+        <View style={styles.container}>
             {/* Image */}
-            <Image style={styles.image} source={{uri: 'https://picsum.photos/200/300'}} />
+            <Image 
+                style={styles.image} 
+                source={{uri: 'https://picsum.photos/400/600'}} 
+            />
             {/* Bed & Bedroom */}
+            <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+
             {/* type & description */}
+            <Text style={styles.description}>Huh Huh Huh its a place to lay down in the dirt</Text>
             {/* old price & new price */}
+            <Text style={styles.prices}>
+                <Text style={styles.oldPrice}>$36</Text>
+                <Text style={styles.newPrice}>$30</Text>
+                / text
+            </Text>
             {/* total price */}
+            <Text style={styles.totalPrice}>$230 total</Text>
         </View>
     );
 };

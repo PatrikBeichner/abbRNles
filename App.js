7ca45b7c091f673d0ed06011aaa590a5ac955894
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-09 15:10:12 -0400

    ScrollView fix and multiple posts + props

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/post';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import feed from './assets/data/feed';
 
-const post1=feed[0];
+const post1 = feed[0];
+const post2 = feed[1];
+const post3 = feed[2];
 
 export default function App() {
   return (
     <SafeAreaView>
-      {/* <HomeScreen /> */}
-      <Post post={post1}/>
+      <ScrollView>
+        {/* <HomeScreen /> */}
+        <Post post={post1} />
+        <Post post={post2} />
+        <Post post={post3} />
+      </ScrollView>
     </SafeAreaView>
   );
 }

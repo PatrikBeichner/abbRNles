5865c02948361dd3af36a28251b4ad397b982914
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-10 14:48:18 -0400

    destination search screen init

---
+++ src/screens/DestinationSearch/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const DestinationSearchScreen =(props) => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default DestinationSearchScreen;
